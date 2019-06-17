8da699ff5f6bcfc13afcb2e1387c46a0d4a8785d
lipc <lipc@lipcdeMacBook-Pro.local>
2019-06-17 17:26:57 +0800

    async code

---
+++ FrontEndEngineer/1_1_1.js
@@ -0,0 +1,20 @@
+/**
+ * JavaScript 规定了几种语言类型
+ * Java​Script 数据类型和数据结构
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Data_structures
+ */
+{
+    /**
+     * 动态类型
+     * JS 的语言特性是动态类型的,这是说明 Js 的变量的数据类型不是一成不变的,是在业务的
+     * 需求中可以发生动态的变化的
+     */
+    let foo = 12;
+    foo = '23';
+    console.log(foo);
+    /**
+     * 最新的 Es 标准定义了 7 种数据类型,其中有 6 种是 primitive 也就是原始类型
+     * Boolean,Null,Undefined,Number,String,Symbol
+     *
+     */
+}
